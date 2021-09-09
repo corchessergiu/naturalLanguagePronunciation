837885ed2b901e747aa646f0713e1c83d3e00405
corchessergiu <corchessergiu194@gmail.com>
2021-09-09 15:06:15 +0300

    final

--- src/nlpSrc.js
+++ src/nlpSrc.js
@@ -1,77 +1,98 @@
+const BN = require('bn.js');
 let numbers = {
     0: "zero", 1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine", 10: "ten", 11: "eleven", 12: "twelve", 13: "thirteen", 14: "fourteen", 15: "fifteen",
     16: "sixteen", 17: "seventeen", 18: "eighteen", 19: "nineteen", 20: "twenty", 30: "thirty", 40: "forty", 50: "fifty", 60: "sixty", 70: "seventy", 80: "eighty", 90: "ninety",
-    100: "hundred", 1000: "thousand", 1000000: "million", 1000000000: "billion"
+    100: "hundred", 1000: "thousand", 1000000: "million", 1000000000: "billion", 1_000_000_000_000: "trillion"
 };
 
 say = function (number) {
-    if (number === 100 || number === 1000 || number === 1000000 || number === 1000000000) {
-        return "one " + numbers[number];
-    }
-    else {
-        if (number in numbers) { //number from 1 to 19 and tens
-            return numbers[number];
-        } else {
-            if (number < 100) { //number from 21 to 99
-                return numbers[number - number % 10] + "-" + numbers[number % 10];
+    if (number < 0 || number > 999999999999999999n || typeof (number) === "string") {
+        return "wrong input"
+    } else {
+        if (number === 100 || number === 1000 || number === 1000000 || number === 1000000000) {
+            return "one " + numbers[number];
+        }
+        else {
+            if (number in numbers) { //number from 1 to 19 and tens
+                return numbers[number];
             } else {
-                if (number < 1000) { //number from 101 to 999
-                    u = number % 10;
-                    z = Math.floor(number % 100 / 10);
-                    s = Math.floor(number % 1000 / 100);
-                    if (s != 0 && z != 0) {
-                        return say(Math.floor(number / 100)) + " " + numbers[100] + " " + say(number % 100);
-                    } else {
-                        if (z != 0) {
-                            return say(Math.floor(number / 100)) + " " + numbers[100] + " " + say(number % 10);
+                if (number < 100) { //number from 21 to 99
+                    return numbers[number - number % 10] + "-" + numbers[number % 10];
+                } else {
+                    if (number < 1000) { //number from 101 to 999
+                        u = number % 10;
+                        z = Math.floor(number % 100 / 10);
+                        s = Math.floor(number % 1000 / 100);
+                        if (s != 0 && z != 0) {
+                            return say(Math.floor(number / 100)) + " " + numbers[100] + " " + say(number % 100);
                         } else {
-                            if (u != 0 && z === 0) {
-                                return say(Math.floor(number / 100)) + " " + numbers[100] + " and " + say(number % 10);
+                            if (z != 0) {
+                                return say(Math.floor(number / 100)) + " " + numbers[100] + " " + say(number % 10);
                             } else {
-                                return say(Math.floor(number / 100)) + " " + numbers[100];
+                                if (u != 0 && z === 0) {
+                                    return say(Math.floor(number / 100)) + " " + numbers[100] + " and " + say(number % 10);
+                                } else {
+                                    return say(Math.floor(number / 100)) + " " + numbers[100];
+                                }
                             }
                         }
-                    }
-                } else {
-                    if (number < 1000000) {    //number from 1001 to 999999            
-                        if (number % 1000 < 10 && number % 1000 != 0) {
-                            return say(Math.floor(number / 1000)) + " " + numbers[1000] + " and " + say(number % 1000);
-                        } if (number % 1000 === 0) {
-                            return say(Math.floor(number / 1000)) + " " + numbers[1000];
-                        }
-                        else {
-                            return say(Math.floor(number / 1000)) + " " + numbers[1000] + " " + say(number % 1000);
-                        }
                     } else {
-                        if (number < 1000000000) { //number from 1_000_001 to 999_999_999
-                            if ((Math.floor(number / 1000) % 1000) === 0 && number % 1000 == 0) {
-                                return say(Math.floor(number / 1000000)) + " " + numbers[1000000];
-                            } else {
-                                if ((Math.floor(number / 1000) % 1000) != 0 && number % 1000 != 0) {
-                                    return say(Math.floor(number / 1000000)) + " " + numbers[1000000] + " " + say(Math.floor(number / 1000) % 1000) + " " + numbers[1000] + " " + say(number % 1000);
+                        if (number < 1_000_000) {    //number from 1001 to 999999     
+                            if (number % 1000 < 10 && number % 1000 != 0) {
+                                return say(Math.floor(number / 1000)) + " " + numbers[1000] + " and " + say(number % 1000);
+                            } if (number % 1000 === 0) {
+                                return say(Math.floor(number / 1000)) + " " + numbers[1000];
+                            }
+                            else {
+                                return say(Math.floor(number / 1000)) + " " + numbers[1000] + " " + say(number % 1000);
+                            }
+                        } else {
+                            if (number < 1_000_000_000) { //number from 1_000_001 to 999_999_999
+                                if ((Math.floor(number / 1000) % 1000) === 0 && number % 1000 == 0) {
+                                    return say(Math.floor(number / 1000000)) + " " + numbers[1000000];
                                 } else {
-                                    if ((Math.floor(number / 1000) % 1000) === 0 && number % 1000 != 0) {
-                                        if (number % 1000 < 10) {
-                                            return say(Math.floor(number / 1000000)) + " " + numbers[1000000] + " and " + say(number % 1000);
-                                        } else {
-                                            return say(Math.floor(number / 1000000)) + " " + numbers[1000000] + " " + say(number % 1000);
-                                        }
+                                    if ((Math.floor(number / 1000) % 1000) != 0 && number % 1000 != 0) {
+                                        return say(Math.floor(number / 1000000)) + " " + numbers[1000000] + " " + say(Math.floor(number / 1000) % 1000) + " " + numbers[1000] + " " + say(number % 1000);
                                     } else {
-                                        if ((Math.floor(number / 1000) % 1000) != 0 && number % 1000 === 0) {
-                                            return say(Math.floor(number / 1000000)) + " " + numbers[1000000] + " " + say(Math.floor(number / 1000) % 1000);
+                                        if ((Math.floor(number / 1000) % 1000) === 0 && number % 1000 != 0) {
+                                            if (number % 1000 < 10) {
+                                                return say(Math.floor(number / 1000000)) + " " + numbers[1000000] + " and " + say(number % 1000000);
+                                            } else {
+                                                return say(Math.floor(number / 1000000)) + " " + numbers[1000000] + " " + say(number % 1000000);
+                                            }
+                                        } else {
+                                            if ((Math.floor(number / 1000) % 1000) != 0 && number % 1000 === 0) {
+                                                return say(Math.floor(number / 1000000)) + " " + numbers[1000000] + " " + say(number % 1000000);
+                                            }
                                         }
                                     }
                                 }
-                            }
-                        } else {
-                            if (number < 1_000_000_000_000) {
-                                if (number % 1000000000 != 0) {
-                                    return say(Math.floor(number / 1000000000)) + " " + numbers[1000000000] + " " + say(number % 1000000000);
+                            } else {
+                                if (number < 1_000_000_000_000) {
+                                    if (number % 1000000000 != 0) {
+                                        return say(Math.floor(number / 1000000000)) + " " + numbers[1000000000] + " " + say(number % 1000000000);
+                                    } else {
+                                        return say(Math.floor(number / 1000000000)) + " " + numbers[1000000000];
+                                    }
                                 } else {
-                                    return say(Math.floor(number / 1000000000)) + " " + numbers[1000000000];
+                                    if (number < 1_000_000_000_000_000) {
+                                        if (number % 1000000000000 != 0) {
+                                            return say(Math.floor(number / 1000000000000)) + " " + numbers[1000000000000] + " " + say(number % 1000000000000);
+                                        } else {
+                                            return say(Math.floor(number / 1000000000000)) + " " + numbers[1000000000000];
+                                        }
+                                    } else {
+                                        var bigNumber = new BN(number);
+                                        var numar = bigNumber.toString().slice();
+                                        var nrToString = bigNumber.toString();
+                                        var lastDigits = numar.slice(numar.length - 12); 
+                                        var nrr = Math.floor(nrToString / 1_000_000_000_000);
+                                        return say(parseInt(nrr)) + " " + numbers[1000000000000] + " " + say(parseInt(lastDigits));
+                                    }
                                 }
                             }
                         }
+
                     }
                 }
             }
--- test/nlpProblem.js
+++ test/nlpProblem.js
@@ -1,143 +1,195 @@
-const {expect} =require("chai");
-const {say}=require("../src/nlpSrc");
+const { expect } = require("chai");
+const { say } = require("../src/nlpSrc");
+const BN = require('bn.js');
 
-describe("Under twenty",function(){
-    it("Should say zero",function(){
+describe("Interval exceded",function(){
+
+    it("Should say exceded", function () {
+        expect(say('-1')).to.equal("wrong input");
+    });
+    it("Should say exceded", function () {
+        var nrr2 = new BN('9999999999999999999');
+        expect(say(nrr2)).to.equal("wrong input");
+    });
+    it("test for string",function(){
+        expect(say("strtest")).to.equal("wrong input")
+    })
+})
+
+describe("Under twenty", function () {
+    it("Should say zero", function () {
         expect(say(0)).to.equal("zero");
     });
-    it("Should say one",function(){
+    it("Should say one", function () {
         expect(say(1)).to.equal("one");
     });
-    it("Should say seven",function(){
+    it("Should say seven", function () {
         expect(say(7)).to.equal("seven");
     });
-    it("Should say sixteen",function(){
+    it("Should say sixteen", function () {
         expect(say(16)).to.equal("sixteen");
     })
 });
 
-describe("Tens",function(){
-    it("Should say thirty-one",function(){
+describe("Tens", function () {
+    it("Should say thirty-one", function () {
         expect(say(31)).to.equal("thirty-one");
     })
-    it("Should say eighty",function(){
+    it("Should say eighty", function () {
         expect(say(80)).to.equal("eighty");
     })
-    it("Should say ninety-three",function(){
+    it("Should say ninety-three", function () {
         expect(say(93)).to.equal("ninety-three");
     })
 })
 
-describe("Hundreds",function(){
-    it("Should say one hundred",function(){
+describe("Hundreds", function () {
+    it("Should say one hundred", function () {
         expect(say(100)).to.equal("one hundred");
     })
-    it("Should say one hundred and five",function(){
+    it("Should say one hundred and five", function () {
         expect(say(105)).to.equal("one hundred and five");
     })
-    it("Should say nine hundred ninety-five",function(){
+    it("Should say nine hundred ninety-five", function () {
         expect(say(995)).to.equal("nine hundred ninety-five");
     })
-    it("Should say two hundred twenty-one",function(){
+    it("Should say two hundred twenty-one", function () {
         expect(say(221)).to.equal("two hundred twenty-one");
     })
-    it("Should say seven hundred and one",function(){
+    it("Should say seven hundred and one", function () {
         expect(say(701)).to.equal("seven hundred and one");
     })
-    it("Should say eight hundred eighty-eight",function(){
+    it("Should say eight hundred eighty-eight", function () {
         expect(say(888)).to.equal("eight hundred eighty-eight");
     })
-    it("Should say one hundred fifteeen",function(){
+    it("Should say one hundred fifteeen", function () {
         expect(say(115)).to.equal("one hundred fifteen");
     })
 })
 
-describe("Thousand",function(){
-    it("Should say one thousand",function(){
+describe("Thousand", function () {
+    it("Should say one thousand", function () {
         expect(say(1000)).to.equal("one thousand");
     })
-    it("Should say one thousand and six",function(){
+    it("Should say one thousand and six", function () {
         expect(say(1006)).to.equal("one thousand and six");
     })
-    it("Should say one thousand nine hundred ninety-nine",function(){
+    it("Should say one thousand nine hundred ninety-nine", function () {
         expect(say(1999)).to.equal("one thousand nine hundred ninety-nine");
     })
-    it("Should say seven thousand eight hundred and one",function(){
+    it("Should say seven thousand eight hundred and one", function () {
         expect(say(7801)).to.equal("seven thousand eight hundred and one");
     })
-    it("Should say eight thousand nine hundred forty",function(){
+    it("Should say eight thousand nine hundred forty", function () {
         expect(say(8940)).to.equal("eight thousand nine hundred forty");
     })
-    it("Should say eight thousand fifty",function(){
+    it("Should say eight thousand fifty", function () {
         expect(say(8050)).to.equal("eight thousand fifty");
     })
-    it("Should say one hundred and thirty-one thousand two hundred eleven",function(){
+    it("Should say one hundred and thirty-one thousand two hundred eleven", function () {
         expect(say(131211)).to.equal("one hundred thirty-one thousand two hundred eleven");
     })
-    it("Should say two hundred thousand",function(){
+    it("Should say two hundred thousand", function () {
         expect(say(200000)).to.equal("two hundred thousand");
     })
-    it("Should say two hundred thousand and one",function(){
+    it("Should say two hundred thousand and one", function () {
         expect(say(200001)).to.equal("two hundred thousand and one");
     })
-    it("Should say seventy thousand and one",function(){
+    it("Should say seventy thousand and one", function () {
         expect(say(70001)).to.equal("seventy thousand and one");
     })
-    it("Should say three hundred and three thousand three hundred and three",function(){
+    it("Should say three hundred and three thousand three hundred and three", function () {
         expect(say(303303)).to.equal("three hundred and three thousand three hundred and three");
     })
 })
 
-describe("Million",function(){
-    it("Should say one million",function(){
+describe("Million", function () {
+    it("Should say one million", function () {
         expect(say(1000000)).to.equal("one million");
     })
-    it("Should say one million and one",function(){
+    it("Should say one million and one", function () {
         expect(say(1000001)).to.equal("one million and one");
     })
-    it("Should say two million",function(){
+    it("Should say two million", function () {
         expect(say(2000000)).to.equal("two million");
     })
-    it("Should say one hundred and sixty million three hundred and forty-three thousand one hundred and one",function(){
+    it("Should say one hundred and sixty million three hundred and forty-three thousand one hundred and one", function () {
         expect(say(160343101)).to.equal("one hundred sixty million three hundred forty-three thousand one hundred and one");
     })
-    it("Should say two hundred million",function(){
+    it("Should say two hundred million", function () {
         expect(say(200000000)).to.equal("two hundred million");
     })
-    it("Should say twenty hundred million",function(){
+    it("Should say twenty hundred million", function () {
         expect(say(20000000)).to.equal("twenty million");
     })
-    it("Should say one hundred and one million one hundred and one thousand one hundred and one",function(){
+    it("Should say one hundred and one million one hundred and one thousand one hundred and one", function () {
         expect(say(101101101)).to.equal("one hundred and one million one hundred and one thousand one hundred and one");
     })
-    it("Should say nine hundred ninety-nine million nine hundred ninety-nine thousand nine hundred ninety-nine",function(){
+    it("Should say nine hundred ninety-nine million nine hundred ninety-nine thousand nine hundred ninety-nine", function () {
         expect(say(999999999)).to.equal("nine hundred ninety-nine million nine hundred ninety-nine thousand nine hundred ninety-nine");
     })
-    it("Should say two hundred and one million one hundred and one",function(){
+    it("Should say two hundred and one million one hundred and one", function () {
         expect(say(201000101)).to.equal("two hundred and one million one hundred and one");
     })
-    it("Should say two hundred million one thousand one hundred and one",function(){
+    it("Should say two hundred million one thousand one hundred and one", function () {
         expect(say(200001101)).to.equal("two hundred million one thousand one hundred and one");
     })
 })
 
-describe("Billion", function(){
-    it("Should say one billion",function(){
+describe("Billion", function () {
+    it("Should say one billion", function () {
         expect(say(1_000_000_000)).to.equal("one billion");
     })
-    it("Should say one hundred billion",function(){
+    it("Should say one hundred billion", function () {
         expect(say(100_000_000_000)).to.equal("one hundred billion");
     })
-    it("Should say one hundred twenty-three billion three hundred thirty-three million one hundred thousand one hundred and one",function(){
+    it("Should say one hundred twenty-three billion three hundred thirty-three million one hundred thousand one hundred and one", function () {
         expect(say(123_333_100_101)).to.equal("one hundred twenty-three billion three hundred thirty-three million one hundred thousand one hundred and one");
     })
-    it("Should say one hundred and one billion three hundred and three million one hundred and six thousand one hundred and one",function(){
+    it("Should say one hundred and one billion three hundred and three million one hundred and six thousand one hundred and one", function () {
         expect(say(101_303_106_101)).to.equal("one hundred and one billion three hundred and three million one hundred and six thousand one hundred and one");
     })
-    it("Should say one hundred and one billion one hundred and one",function(){
+    it("Should say one hundred and one billion one hundred and one", function () {
         expect(say(101_000_000_101)).to.equal("one hundred and one billion one hundred and one");
     })
-    it("Should say thirty billion nine hundred ninety-nine million nine hundred ninety-nine thousand nine hundred and nine",function(){
+    it("Should say thirty billion nine hundred ninety-nine million nine hundred ninety-nine thousand nine hundred and nine", function () {
         expect(say(30_999_999_909)).to.equal("thirty billion nine hundred ninety-nine million nine hundred ninety-nine thousand nine hundred and nine");
+    })
+})
+
+describe("Trillion", function () {
+    it("Should say three trillion", function () {
+        expect(say(3_000_000_000_000)).to.equal("three trillion");
+    })
+    it("Should say three trillion one thousand", function () {
+        expect(say(3_000_000_001_000)).to.equal("three trillion one thousand");
+    })
+    it("Should say three trillion fifty million one hundred and one thousand", function () {
+        expect(say(3_000_050_101_000)).to.equal("three trillion fifty million one hundred and one thousand");
+    })
+    it("Should say three hundred trillion fifty million one hundred and one thousand", function () {
+        expect(say(300_000_050_101_000)).to.equal("three hundred trillion fifty million one hundred and one thousand");
+    })
+    it("Should say three hundred trillion one billion fifty million one hundred and one thousand", function () {
+        expect(say(300_001_050_101_000)).to.equal("three hundred trillion one billion fifty million one hundred and one thousand");
+    })
+})
+describe("Over Trillion", function () {
+    it("Should say three thousand two hundred thirty trillion one hundred eleven billion two hundred twenty-two million three hundred thirty-three thousand four hundred forty-four", function () {
+        expect(say(3230111222333444)).to.equal("three thousand two hundred thirty trillion one hundred eleven billion two hundred twenty-two million three hundred thirty-three thousand four hundred forty-four");
+    })
+    var nr = new BN('31230111222333444');
+    it("Should say thirty-one thousand two hundred thirty trillion one hundred eleven billion two hundred twenty-two million three hundred thirty-three thousand four hundred forty-four", function () {
+        expect(say(nr)).to.equal("thirty-one thousand two hundred thirty trillion one hundred eleven billion two hundred twenty-two million three hundred thirty-three thousand four hundred forty-four");
+    })
+
+    it("Should say thirty-one thousand two hundred thirty trillion one hundred eleven billion two hundred twenty-two million three hundred thirty-three thousand four hundred forty", function () {
+        var nrr = new BN('31230111222333440');
+        expect(say(nrr)).to.equal("thirty-one thousand two hundred thirty trillion one hundred eleven billion two hundred twenty-two million three hundred thirty-three thousand four hundred forty");
+    })
+
+    it("Should say thirty-one thousand two hundred thirty trillion one hundred eleven billion two hundred twenty-two million three hundred thirty-three thousand four hundred forty-nine", function () {
+        var nrr2 = new BN('31230111222333449');
+        expect(say(nrr2)).to.equal("thirty-one thousand two hundred thirty trillion one hundred eleven billion two hundred twenty-two million three hundred thirty-three thousand four hundred forty-nine");
     })
 })
